0aa4489431dbf68ed78f8a105fe1b919ecd3d59f
61hhh <1587403870@qq.com>
2020-07-31 23:58:07 +0800

    Site updated: 2020-07-31 23:57:56

--- js/funnyTitle.js
+++ js/funnyTitle.js
@@ -1,17 +1,22 @@
 // <!--浏览器搞笑标题-->
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+/* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
+window.onload = function() {
+    var OriginTitile = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function() {
+      if(document.hidden) {
+        $('[rel="icon"]').attr('href', "/failure.ico");
+        $('[rel="shortcut icon"]').attr('href', "/failure.ico");
+        document.title = '喔唷，崩溃啦！';
+        clearTimeout(titleTime);
+      } else {
+        $('[rel="icon"]').attr('href', "/ayer.svg");
+        $('[rel="shortcut icon"]').attr('href', "/ayer.svg");
+        document.title = '咦，页面又好了！';
+        titleTime = setTimeout(function() {
+          document.title = OriginTitile;
+        }, 2000);
+      }
+    });
+  }
+  
